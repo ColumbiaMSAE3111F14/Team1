7d79e51e90fa293083205a13b926a9064cc92a21
Sean <sean.ballinger@gmail.com>
2014-11-16 18:14:38 -0500

    add spin_periodic method

--- Assignment4/ising.py
+++ Assignment4/ising.py
@@ -1,2 +1,6 @@
 import array_plot
 
+def spin_periodic(array, x_coord, y_coord):
+    '''Part 2: return spin in a cell, wrapping around if out of bounds.'''
+    side = array.shape[0]
+    return array[x_coord % size][y_coord % size]
