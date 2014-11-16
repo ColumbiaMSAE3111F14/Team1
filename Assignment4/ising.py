eb1c85136a43940d3362b48e99db82f19676aba1
Sean <sean.ballinger@gmail.com>
2014-11-16 18:23:58 -0500

    fix spin_periodic typo and work with non-square arrays

--- Assignment4/ising.py
+++ Assignment4/ising.py
@@ -2,5 +2,5 @@ import array_plot
 
 def spin_periodic(array, x_coord, y_coord):
     '''Part 2: return spin in a cell, wrapping around if out of bounds.'''
-    side = array.shape[0]
-    return array[x_coord % size][y_coord % size]
+    (side_x, side_y) = array.shape
+    return array[x_coord % side_x][y_coord % side_y]
